a047446c004729f66900cac9774dcd92a78c6000
habiba <hmokhtar927@gmail.com>
2024-12-15 22:38:09 +0200

    Add

--- src/main/java/app_system/orders/Order.java
+++ src/main/java/app_system/orders/Order.java
@@ -8,22 +8,24 @@ import java.util.ArrayList;
 
 public class Order {
 
-    // allergies or preferences(spicy/well-done)?
-    private final User customer;
-    private final Restaurant restaurant;
+    private  User customer;
+    private Restaurant restaurant;
     private ArrayList <Cart_Item> order_items;
-    private String deliveryAddress;
-    private Date delivery_time; //not final due to delay or order change?
+    private String deliveryAddress,deliverytime;
     private Payment payment_info;
 
 
-    public Order(User customer, Restaurant restaurant, ArrayList<Cart_Item> order_items, String deliveryAddress,Date delivery_time,Payment payment_info){
+    public Order(User customer, Restaurant restaurant, ArrayList<Cart_Item> order_items,Payment payment_info){
         this.customer=customer;
         this.restaurant=restaurant;
         this.order_items=order_items;
-        this.deliveryAddress=deliveryAddress;
-        this.delivery_time=delivery_time;
         this.payment_info=payment_info;
+
+    }
+
+    public Order( String deliveryAddress,String deliverytime){
+        this.deliveryAddress=deliveryAddress;
+        this.deliverytime=deliverytime;
 
     }
 
@@ -39,9 +41,6 @@ public class Order {
         return deliveryAddress;
     }
 
-    public Date getDelivery_time() {
-        return delivery_time;
-    }
 
     public Payment getPayment_info() {
         return payment_info;
@@ -55,17 +54,20 @@ public class Order {
         this.deliveryAddress = deliveryAddress;
     }
 
-    public void setDelivery_time(Date delivery_time) {
-        this.delivery_time = delivery_time;
-    }
-
     public void setPayment_info(Payment payment_info) {
         this.payment_info = payment_info;
     }
 
-
     public void setOrder_items(ArrayList<Cart_Item> order_items) {
         this.order_items= order_items;
+    }
+
+    public void setRestaurant(Restaurant restaurant) {
+        this.restaurant= restaurant;
+    }
+
+    public void setCustomer(User customer) {
+        this.customer= customer;
     }
 
 
