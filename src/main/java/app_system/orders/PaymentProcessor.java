e85a9370bd428f3bf67289dabacf33ffb740165a
mennaproject <163206956+mennaproject@users.noreply.github.com>
2024-12-16 02:30:33 +0200

    updates

--- src/main/java/app_system/orders/PaymentProcessor.java
+++ src/main/java/app_system/orders/PaymentProcessor.java
@@ -1,20 +1,26 @@
 package app_system.orders;
-import app_system.restaurants.Menu_Item;
+//import app_system.restaurants.Menu_Item;
 import java.util.Random;
 
 public class PaymentProcessor {
 
-    public double calculateAmount(Menu_Item selectedItem) {
-        if (selectedItem != null) {
-            double amount = selectedItem.getPrice();
-            System.out.println("Amount calculated: $" + amount);
-            return amount;
-        } else {
-            System.out.println("Item not found.");
-            return 0.0;
+//    public double calculateAmount(Menu_Item selectedItem) {
+//        if (selectedItem != null) {
+//            double amount = selectedItem.getPrice();
+//            System.out.println("Amount calculated: $" + amount);
+//            return amount;
+//        } else {
+//            System.out.println("Item not found.");
+//            return 0.0;
+//        }
+//    }
+    private double calculateAmount(Order order) {
+        double amount = 0;
+        for (Cart_Item item : order.getOrder_items()) {
+            amount += item.getMenu_item().getPrice() * item.getQuantity();
         }
+        return amount;
     }
-
     public int generateTransactionID() {
         int transactionID = new Random().nextInt(100000);
         System.out.println("Payment process Transaction ID: " + transactionID);
