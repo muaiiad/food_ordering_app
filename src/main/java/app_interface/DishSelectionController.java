0174a84f91177bdad2ec7988be23a4f46be217b6
MEnna-zayed <mennamohammad496@gmail.com>
2024-12-16 05:41:40 +0200

    gui of menu view and dish selection

--- src/main/java/app_interface/DishSelectionController.java
+++ src/main/java/app_interface/DishSelectionController.java
@@ -1,4 +1,33 @@
 package app_interface;
 
-public class DishSelectionController {
-}
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+
+    public class DishSelectionController {
+
+        @FXML
+        private Button itemPrice;
+        @FXML
+        private Label itemDis;
+        @FXML
+        private Label itemName;
+        public void initialize ()
+        {
+            setLabel();
+        }
+
+        public void setLabel ()
+        {
+            itemName.setText(app_system.restaurants.RestaurantManager.getRestaurants().get(0).getMenu().getItems().get(0).getName());
+            itemDis.setText(app_system.restaurants.RestaurantManager.getRestaurants().get(0).getMenu().getItems().get(0).getDescription());
+            itemPrice.setText("Make 1 required selection -$"+app_system.restaurants.RestaurantManager.getRestaurants().get(0).getMenu().getItems().get(0).getPrice());
+        }
+
+    }
+
